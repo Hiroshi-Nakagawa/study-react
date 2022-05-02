51f0472f55364b808ae948d9cfd1cdb0b23b596f
HiroshiNakagawa <rosshi96@gmail.com>
2022-05-02 17:32:10 +0900

    ⚗️ useEffectを試しに実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,17 @@ import { Footer } from "src/components/Footer";
 import classes from "src/styles/Home.module.css";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
-  const foo = 1;
-
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  },[]);
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
 
   return (
     <div className={classes.container}>
@@ -20,7 +21,6 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>ボタン</a>
       <Main page="index" />
       <Footer />
     </div>
