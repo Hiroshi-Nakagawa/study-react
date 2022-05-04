19fa74d86c2a8569eff8acbc430cd846b16d6c99
HiroshiNakagawa <rosshi96@gmail.com>
2022-05-05 07:41:00 +0900

    ✨ インプットテキストの制御機能、カウントの表示・非表示の制御機能を実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,13 +7,27 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     console.log(count);
     if(count < 10) {
       setCount(count => count + 1);
     }
-  }, [count])
+  }, [count]);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  } ,[]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -29,7 +43,9 @@ export default function Home() {
       </Head>
       <Header />
       <button onClick={handleClick}>ボタン</button>
-      {count}
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      {isShow ? <h1>{count}</h1> : null}
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
