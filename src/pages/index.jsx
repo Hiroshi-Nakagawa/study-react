ec4c0f6fa50ab68663ad98d8c4a7c2b0f5f2873b
HiroshiNakagawa <rosshi96@gmail.com>
2022-05-05 06:46:18 +0900

    ⚡️ useCallback、useEffectの第二引数に変数を入れてパフォーマンス向上させた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,21 +3,24 @@ import { Footer } from "src/components/Footer";
 import classes from "src/styles/Home.module.css";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  }
+
+  const handleClick = useCallback(() => {
+    console.log(count);
+    if(count < 10) {
+      setCount(count => count + 1);
+    }
+  }, [count])
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [count]);
 
   return (
     <div className={classes.container}>
