bcecb8a2c8afdb7f6c9afebe060bf59edfd1d923
HiroshiNakagawa <rosshi96@gmail.com>
2022-05-06 14:25:17 +0900

    🐛 カスタムフックスの出力を修正した

--- src/hooks/useInputArray.jsx
+++ src/hooks/useInputArray.jsx
@@ -1,26 +1,26 @@
 import { useCallback, useState } from "react";
 
 export const useInputArray = () => {
-    const [text, setText] = useState("");
-    const [array, setArray] = useState([]);
+  const [text, setText] = useState("");
+  const [array, setArray] = useState([]);
 
-    const handleChange = useCallback((e) => {
-        if (e.target.value.length > 5) {
-            alert("5文字以内にしてください");
-            return;
-        }
-        setText(e.target.value.trim());
-    } ,[]);
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
 
-    const handleAdd = useCallback(() => {
+  const handleAdd = useCallback(() => {
     setArray((prevArray) => {
-        if (prevArray.some((item) => item === text)) {
-        alert("同じ要素が既に存在します。")
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素が既に存在します。");
         return prevArray;
-        }
-        return [...prevArray, text];
+      }
+      return [...prevArray, text];
     });
-    }, [text]);
+  }, [text]);
 
-    return [text, array, handleChange, handleAdd];
-}
+  return { text, array, handleChange, handleAdd };
+};
