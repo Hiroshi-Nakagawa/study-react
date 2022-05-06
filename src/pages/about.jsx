49c87ebfa8c5d743690dba4f0102631da305afe4
HiroshiNakagawa <rosshi96@gmail.com>
2022-05-06 15:32:24 +0900

    ✨ useMemoの例を学んだ

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,7 +6,7 @@ import classes from "src/styles/Home.module.css";
 
 export default function About(props) {
   const {
-    count,
+    doubleCount,
     isShow,
     handleClick,
     handleDisplay,
@@ -24,7 +24,7 @@ export default function About(props) {
       <Header />
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-      {isShow ? <h1>{count}</h1> : null}
+      {isShow ? <h1>{doubleCount}</h1> : null}
       <input type="text" value={text} onChange={handleChange} />
       <button onClick={handleAdd}>要素追加</button>
       <ul>
