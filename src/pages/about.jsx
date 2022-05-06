b3c1be7cbad05ae64bff9d317aabb23cf9025b2e
HiroshiNakagawa <rosshi96@gmail.com>
2022-05-06 14:28:55 +0900

    ⚡️ ページ間のStateをリフトアップさせて_app.jsxで管理するようにした

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,14 +3,18 @@ import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import classes from "src/styles/Home.module.css";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
-export default function About() {
-  const [count, isShow, handleClick, handleDisplay] = useCounter();
-  const [text, array, handleChange, handleAdd] = useInputArray();
-  useBgLightBlue();
+export default function About(props) {
+  const {
+    count,
+    isShow,
+    handleClick,
+    handleDisplay,
+    text,
+    array,
+    handleChange,
+    handleAdd,
+  } = props;
 
   return (
     <div className={classes.container}>
@@ -25,7 +29,7 @@ export default function About() {
       <button onClick={handleAdd}>要素追加</button>
       <ul>
         {array.map((item) => {
-          return (<li key={item}>{item}</li>)
+          return <li key={item}>{item}</li>;
         })}
       </ul>
       <Main page="about" />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,18 @@ import { Footer } from "src/components/Footer";
 import classes from "src/styles/Home.module.css";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
-export default function Home() {
-  const [count, isShow, handleClick, handleDisplay] = useCounter();
-  const [text, array, handleChange, handleAdd] = useInputArray();
-  useBgLightBlue();
+export default function Home(props) {
+  const {
+    count,
+    isShow,
+    handleClick,
+    handleDisplay,
+    text,
+    array,
+    handleChange,
+    handleAdd,
+  } = props;
 
   return (
     <div className={classes.container}>
@@ -25,7 +29,7 @@ export default function Home() {
       <button onClick={handleAdd}>要素追加</button>
       <ul>
         {array.map((item) => {
-          return (<li key={item}>{item}</li>)
+          return <li key={item}>{item}</li>;
         })}
       </ul>
       <Main page="index" />
