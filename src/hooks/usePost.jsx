2bc10905893f37f74e76558061d92e7fb9b33a5f
HiroshiNakagawa <rosshi96@gmail.com>
2022-05-10 15:40:10 +0900

    ✨ ユーザーのfetch、fetcherの共通化

---
+++ src/hooks/usePost.jsx
@@ -0,0 +1,26 @@
+import { fetcher } from "src/util/fetcher";
+const { useRouter } = require("next/router");
+const { default: useSWR } = require("swr");
+
+export const usePost = () => {
+  const router = useRouter();
+  const { data: post, error: postError } = useSWR(
+    router.query.id
+      ? `https://jsonplaceholder.typicode.com/posts/${router.query.id}`
+      : null,
+    fetcher
+  );
+  const { data: user, error: userError } = useSWR(
+    post?.userId
+      ? `https://jsonplaceholder.typicode.com/users/${post.userId}`
+      : null,
+    fetcher
+  );
+
+  return {
+    post,
+    user,
+    error: postError || userError,
+    isLoading: !postError && !user && !userError, // 講座では!postErrorは書いていない
+  };
+};
